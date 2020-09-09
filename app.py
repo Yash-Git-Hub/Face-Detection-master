511d9d3bbc2224538df0dd6a3ea6f74176935b27
Yash-Git-Hub <69065839+Yash-Git-Hub@users.noreply.github.com>
2020-09-09 16:49:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-# Importing required libraries, obviously
-import streamlit as st
+# Core Pkgs
+import streamlit as st 
 import cv2
-from PIL import Image
-import numpy as np
+from PIL import Image,ImageEnhance
+import numpy as np 
 import os
+our_image=None
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
-
-# Loading pre-trained parameters for the cascade classifier
 try:
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
@@ -14,111 +14,146 @@ try:
 except Exception:
     st.write("Error loading cascade classifiers")
 
-def detect(image):
-    '''
-    Function to detect faces/eyes and smiles in the image passed to this function
-    '''
 
-    
-    image = np.array(image.convert('RGB'))
-    
-    # Next two lines are for converting the image from 3 channel image (RGB) into 1 channel image
-    # img = cv2.cvtColor(new_img, 1)
-    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    # Passing grayscale image to perform detection
-    # We pass grayscaled image because opencv expects image with one channel
-    # Even if you don't convert the image into one channel, open-cv does it automatically.
-    # So, you can just comment line number 26 and 27.
-    # If you do, make sure that you change the variables name at appropriate places in the code below
-    # Don't blame me if you run into errors while doing that :P
-    
-    faces = face_cascade.detectMultiScale(image=image, scaleFactor=1.3, minNeighbors=5)
-    # The face_cascade classifier returns coordinates of the area in which the face might be located in the image
-    # These coordinates are (x,y,w,h)
-    # We will be looking for eyes and smile within this area instead of looking for them in the entire image
-    # This makes sense when you're looking for smiles and eyes in a face, if that is not your use case then
-    # you can pull the code segment out and make a different function for doing just that, specifically.
-
-
-    # Draw rectangle around faces
+def detect_faces(our_image):
+    new_img = np.array(our_image.convert('RGB'))
+    img = cv2.cvtColor(new_img,1)
+    gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.4, 6)
+    # Draw rectangle around the faces
     for (x, y, w, h) in faces:
-        
-        # The following are the parameters of cv2.rectangle()
-        # cv2.rectangle(image_to_draw_on, start_point, end_point, color, line_width)
-        cv2.rectangle(img=image, pt1=(x, y), pt2=(x + w, y + h), color=(255, 0, 0), thickness=2)
-        
-        roi = image[y:y+h, x:x+w]
-        
-        # Detecting eyes in the face(s) detected
-        eyes = eye_cascade.detectMultiScale(roi)
-        
-        # Detecting smiles in the face(s) detected
-        smile = smile_cascade.detectMultiScale(roi, minNeighbors = 25)
-        
-        # Drawing rectangle around eyes
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi, (ex, ey), (ex+ew, ey+eh), (0,255,0), 2)
-            
-        # Drawing rectangle around smile
-        for (sx,sy,sw,sh) in smile:
-            cv2.rectangle(roi, (sx, sy), (sx+sw, sy+sh), (0,0,255), 2)
-
-    # Returning the image with bounding boxes drawn on it (in case of detected objects), and faces array
-    return image, faces
+                cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    return img,faces 
 
 
-def about():
-	st.write(
-		'''
-		**Haar Cascade** is an object detection algorithm.
-		It can be used to detect objects in images or videos. 
+def detect_eyes(our_image):
+    new_img = np.array(our_image.convert('RGB'))
+    img = cv2.cvtColor(new_img,1)
+    gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+    eyes = eye_cascade.detectMultiScale(gray, 1.4, 5)
+    for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(img,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    return img
 
-		The algorithm has four stages:
+def detect_smiles(our_image):
+    new_img = np.array(our_image.convert('RGB'))
+    img = cv2.cvtColor(new_img,1)
+    gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+    # Detect Smiles
+    smiles = smile_cascade.detectMultiScale(gray, 1.4, 6)
+    # Draw rectangle around the Smiles
+    for (x, y, w, h) in smiles:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    return img
 
-			1. Haar Feature Selection 
-			2. Creating  Integral Images
-			3. Adaboost Training
-			4. Cascading Classifiers
+def cartonize_image(our_image):
+    new_img = np.array(our_image.convert('RGB'))
+    img = cv2.cvtColor(new_img,1)
+    gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+    # Edges
+    gray = cv2.medianBlur(gray, 5)
+    edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+    #Color
+    color = cv2.bilateralFilter(img, 9, 300, 300)
+    #Cartoon
+    cartoon = cv2.bitwise_and(color, color, mask=edges)
+
+    return cartoon
 
 
-
-Read more :point_right: https://docs.opencv.org/2.4/modules/objdetect/doc/cascade_classification.html
-https://sites.google.com/site/5kk73gpu2012/assignment/viola-jones-face-detection#TOC-Image-Pyramid
-		''')
-
+def cannize_image(our_image):
+    new_img = np.array(our_image.convert('RGB'))
+    img = cv2.cvtColor(new_img,1)
+    img = cv2.GaussianBlur(img, (11, 11), 0)
+    canny = cv2.Canny(img, 100, 150)
+    return canny
 
 def main():
-    st.title("Face Detection App :sunglasses: ")
-    st.write("**Using the Haar cascade Classifiers**")
+    """Face Detection App"""
+    st.title("Face Detection App")
+    st.text("Build with Streamlit and OpenCV")
 
-    activities = ["Home", "About"]
-    choice = st.sidebar.selectbox("Pick something fun", activities)
+    activities = ["Detection","About"]
+    choice = st.sidebar.selectbox("Select Activty",activities)
 
-    if choice == "Home":
+    if choice == 'Detection':
+        st.subheader("Face Detection")
 
-    	st.write("Go to the About section from the sidebar to learn more about it.")
-        
-        # You can specify more file types below if you want
-    	image_file = st.file_uploader("Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
+        image_file = st.file_uploader("Upload Image",type=['jpg','png','jpeg'])
+        if image_file is not None :
+            our_image = Image.open(image_file)
+            st.text("Original Image")
+            # st.write(type(our_image))
+            st.image(our_image)
+            enhance_type = st.sidebar.radio("Enhance Type",["Original","Gray-Scale","Contrast","Brightness","Blurring"])
 
-    	if image_file is not None:
+            if enhance_type == 'Gray-Scale':
+                new_img = np.array(our_image.convert('RGB'))
+                img = cv2.cvtColor(new_img,1)
+                gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+                # st.write(new_img)
+                st.image(gray)
+            elif enhance_type == 'Contrast':
+                c_rate = st.sidebar.slider("Contrast",0.5,3.5)
+                enhancer = ImageEnhance.Contrast(our_image)
+                img_output = enhancer.enhance(c_rate)
+                st.image(img_output)
 
-    		image = Image.open(image_file)
+            elif enhance_type == 'Brightness':
+                c_rate = st.sidebar.slider("Brightness",0.5,3.5)
+                enhancer = ImageEnhance.Brightness(our_image)
+                img_output = enhancer.enhance(c_rate)
+                st.image(img_output)
 
-    		if st.button("Process"):
-                
-                # result_img is the image with rectangle drawn on it (in case there are faces detected)
-                # result_faces is the array with co-ordinates of bounding box(es)
-    			result_img, result_faces = detect(image=image)
-    			st.image(result_img, use_column_width = True)
-    			st.success("Found {} faces\n".format(len(result_faces)))
-
-    elif choice == "About":
-    	about()
-
+            elif enhance_type == 'Blurring':
+                new_img = np.array(our_image.convert('RGB'))
+                blur_rate = st.sidebar.slider("Brightness",0.5,3.5)
+                img = cv2.cvtColor(new_img,1)
+                blur_img = cv2.GaussianBlur(img,(11,11),blur_rate)
+                st.image(blur_img)
+            
+            elif enhance_type == 'Original':
+                st.image(our_image,width=300)
 
 
+            # Face Detection
+            task = ["Faces","Smiles","Eyes","Cannize","Cartonize"]
+            feature_choice = st.sidebar.selectbox("Find Features",task)
+            if st.button("Process"):
 
-if __name__ == "__main__":
-    main()
+                if feature_choice == 'Faces':
+                    result_img,result_faces = detect_faces(our_image)
+                    st.image(result_img)
+
+                    st.success("Found {} faces".format(len(result_faces)))
+                elif feature_choice == 'Smiles':
+                    result_img = detect_smiles(our_image)
+                    st.image(result_img)
+
+
+                elif feature_choice == 'Eyes':
+                    result_img = detect_eyes(our_image)
+                    st.image(result_img)
+
+                elif feature_choice == 'Cartonize':
+                    result_img = cartonize_image(our_image)
+                    st.image(result_img)
+
+                elif feature_choice == 'Cannize':
+                    result_canny = cannize_image(our_image)
+                    st.image(result_canny)
+                elif feature_choice == 'Cannize':
+                    result_canny = cannize_image(our_image)
+                    st.image(result_canny)
+
+
+
+
+    elif choice == 'About':
+        st.subheader("About Face Detection App")
+        st.markdown("Built with Streamlit by [Yash Pathania](https://www.linkedin.com/in/yash-pathania-03a0301b4/)")
+        st.text("Yash Pathania (Yash)")
+
+if __name__ == '__main__':
+        main()	
